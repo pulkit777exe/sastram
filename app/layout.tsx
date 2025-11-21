ee7a8b7fcff0a7f192b4b197fb9b1fe0bc9b6396
pulkit777exe <pulkitpareek88@gmail.com>
2025-11-21 10:48:02 +0530

    feat: added Analytics from vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import { Providers } from "@/components/providers";
+import { Analytics } from "@vercel/analytics/next"
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
     <html lang="en">
       <body 
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Analytics/>
         <Providers>
           {children}
         </Providers>
